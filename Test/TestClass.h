0af9e95febba3e639cf0934d2a61fc5393751f36
Arul Ajmani <arulajmani@v1022-rn-192-121.campus-dynamic.uwaterloo.ca>
2016-01-07 20:09:40 -0500

    Added Class

---
+++ Test/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Test
+//
+//  Created by Arul Ajmani on 2016-01-07.
+//  Copyright © 2016 Michael Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ Test/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Test
+//
+//  Created by Arul Ajmani on 2016-01-07.
+//  Copyright © 2016 Michael Li. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
