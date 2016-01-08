5f1899d1891e66136f5c6359b97657cc36ceae2e
Michael Li <li.michael@live.com>
2016-01-07 20:01:44 -0500

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Michael Li on 1/7/16.
+//  Copyright © 2016 Michael Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Test/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Test
+//
+//  Created by Michael Li on 1/7/16.
+//  Copyright © 2016 Michael Li. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
